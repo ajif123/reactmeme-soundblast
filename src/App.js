3e66ac5dee58c73793cd0383d0235d5c88c1dfea
ajif123 <101539983+ajif123@users.noreply.github.com>
2023-10-10 16:35:33 +0800

    Completed V0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import logo from "./img/logo.png"
+import data from './data';
+import { useState } from "react";
+import Character from "./Components/Character";
 
 
 function App() {
+  const [characters] = useState(data);
+  const [activeChar, setActiveChar] = useState(characters[0]);
+
+
   return (
     <div className="App">
-      
+      <div className="logo">
+        <img src={logo} alt="Meme Logo"/>
+      </div>
+      <ul className="charaters-con">
+        {
+          characters.map((char) => {
+              const {id, image} = char
+              return <li key={id} className = {`${activeChar.id === id ? 'active' : ''}`} onClick={ () => setActiveChar(char) }>
+                <div className="characters">
+                  <img src={image} alt=""/>
+                </div>
+              </li>
+
+          })
+        }
+      </ul>
+      <Character {...activeChar} />
     </div>
   );
 }
--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './character.css'
 
-function Character() {
+function Character({name, sounds, soundNames}) {
+  const [audio] = useState(new Audio());
+
+  const handleAudioPlay = (e) => {
+    audio.src = e.target.value
+    audio.play()
+  }
+
   return (
-    <div>Character</div>
+    <div className="SoundsCon">
+      <div className="NameCon">
+          <h2>{name}</h2>
+      </div>
+      <div className="Sounds">
+        {
+          sounds.map((sound, index) => {
+            return <button className="SoundsBtn" value={sound} key={index} onClick={handleAudioPlay}>{soundNames[index]}</button>
+          })
+        }
+        {sounds.length === 0 && <p>No Sounds Available for <span>{name}</span></p>}
+      </div>
+    </div>
   )
 }
 
